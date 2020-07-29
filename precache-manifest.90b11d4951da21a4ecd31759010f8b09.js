b61a0417e0929543511de2a007e2a3776264e2eb
Denis Shulgin <d.a.shulgin2019@ya.ru>
2020-07-29 10:11:05 +0300

    Updates

---
+++ precache-manifest.90b11d4951da21a4ecd31759010f8b09.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e7473f329a8f6d8c33b4981333960a",
+    "url": "/MySummary/index.html"
+  },
+  {
+    "revision": "cf05466ab38cb354b62a",
+    "url": "/MySummary/static/css/main.2198faa4.chunk.css"
+  },
+  {
+    "revision": "38fb4a0aea668e0ed690",
+    "url": "/MySummary/static/js/2.b21d8e68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MySummary/static/js/2.b21d8e68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf05466ab38cb354b62a",
+    "url": "/MySummary/static/js/main.ab4fb728.chunk.js"
+  },
+  {
+    "revision": "f3c9dcff8aed78389024",
+    "url": "/MySummary/static/js/runtime-main.a8738240.js"
+  },
+  {
+    "revision": "17a6652f345bc5ea2db2d4fba0c95a84",
+    "url": "/MySummary/static/media/github.17a6652f.png"
+  },
+  {
+    "revision": "d71af0b2a733f0f8f43dedad641a211b",
+    "url": "/MySummary/static/media/reactJS.d71af0b2.png"
+  },
+  {
+    "revision": "7bb145d1e62140f41a4423af67ffca55",
+    "url": "/MySummary/static/media/ya.7bb145d1.png"
+  },
+  {
+    "revision": "8858de8c1ad667c9d5fa9e31969ee91e",
+    "url": "/MySummary/static/media/ава.8858de8c.png"
+  }
+]);
